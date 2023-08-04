d2f2435f86750720402309670bb937b12334ddce
Andrew Urom <andrew.urom@africaprudential.com>
2023-08-04 05:07:21 +0100

    feat: Implemented user maanagement view list

--- src/constants/appRoute.tsx
+++ src/constants/appRoute.tsx
@@ -2,7 +2,8 @@ const APP_ROUTES = {
   dashboard: "/dashboard",
   registrar: "/registrar",
   banks: "/master-data/banks",
-  document_formats: "/master-data/document-formats"
+  document_formats: "/master-data/document-formats",
+  users: "/users",
 };
 
 export default APP_ROUTES;
